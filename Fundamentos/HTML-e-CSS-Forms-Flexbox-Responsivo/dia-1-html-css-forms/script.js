f7b7b71451575ec8d85628b823bb8388d9c78a8e
MarcilioamiL <marcilioaugusto53@gmail.com>
2022-01-04 10:31:46 -0300

    exercici1 bloco6

--- Fundamentos/HTML-e-CSS-Forms-Flexbox-Responsivo/dia-1-html-css-forms/script.js
+++ Fundamentos/HTML-e-CSS-Forms-Flexbox-Responsivo/dia-1-html-css-forms/script.js
@@ -1,4 +1,31 @@
-// SELETORES
-const INPUT_TEXT = document.querySelector("#input-text");
-const INPUT_CHECKBOX = document.querySelector("#input-checkbox");
-const HREF_LINK = document.querySelector("#href");
+const inpText = document.querySelector("#input-text");
+const inpBox = document.querySelector("#input-checkbox");
+const aHref = document.querySelector("#href");
+
+function a (event){
+    event.preventDefault();
+    console.log(aHref.value)
+}
+aHref.addEventListener('click', a);
+
+
+function box (e) {
+    e.preventDefault();
+    console.log(inpBox.value)
+}
+inpBox.addEventListener('click', box);
+
+
+
+function texto (e) {
+    let letra = e.key;
+    if (letra !== 'a'){
+        e.preventDefault();
+        console.log(inpText.value)
+    }
+}
+inpText.addEventListener('keypress', texto);
+
+
+
+
